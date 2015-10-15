211aa96f3f63ce26107a2f53126a71e02e94f249
Nguyen Anh Quynh <aquynh@gmail.com>
2015-10-16 01:14:57 +0800

    version 0.9

---
+++ samples/test1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from unicorn import *
+from unicorn.x86_const import *
+
+# code to be emulated
+X86_CODE32 = b"\x41\x4a" # INC ecx; DEC dex
+
+# memory address where emulation starts
+ADDRESS = 0x1000000
+
+print("Emulate i386 code")
+try:
+    # Initialize emulator in X86-32bit mode
+    mu = Uc(UC_ARCH_X86, UC_MODE_32)
+
+    # map 2MB memory for this emulation
+    mu.mem_map(ADDRESS, 2 * 1024 * 1024)
+
+    # write machine code to be emulated to memory
+    mu.mem_write(ADDRESS, X86_CODE32)
+
+    # initialize machine registers
+    mu.reg_write(UC_X86_REG_ECX, 0x1234)
+    mu.reg_write(UC_X86_REG_EDX, 0x7890)
+
+    # emulate code in infinite time & unlimited instructions
+    mu.emu_start(ADDRESS, ADDRESS + len(X86_CODE32))
+
+    # now print out some registers
+    print("Emulation done. Below is the CPU context")
+
+    r_ecx = mu.reg_read(UC_X86_REG_ECX)
+    r_edx = mu.reg_read(UC_X86_REG_EDX)
+    print(">>> ECX = 0x%x" %r_ecx)
+    print(">>> EDX = 0x%x" %r_edx)
+
+except UcError as e:
+    print("ERROR: %s" % e)
+
